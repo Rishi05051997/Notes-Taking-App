48bfccc406dc5f0d3656d3d91955c385f316ec0a
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-04-18 08:33:32 +0530

    Feat : Routing On Live

---
+++ src/private-router/Private-Rounter.js
@@ -0,0 +1,8 @@
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider"
+
+export const PrivateRouter = ({ children }) => {
+    const { login } = useAuth();
+    let location = useLocation();
+    return login ? children : < Navigate to="/login" state={{ from: location }} replace />
+}
