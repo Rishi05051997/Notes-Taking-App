01e045dcdf6aa68a2c1eb5cae1f93978b8071f50
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-05-29 07:41:58 +0530

    Fixed : Change colors to hexcode & created saperate file for pallet data

--- src/components/ColorPallet/ColorPallet.jsx
+++ src/components/ColorPallet/ColorPallet.jsx
@@ -1,38 +1,7 @@
 import { useData } from "../../context/DataProvider"
 import { updateNote } from "../../services";
+import { colorPalletData } from "./palletData"
 
-const colorPalletData = [
-    {
-        id: 1,
-        cssName: "red",
-        bgProp: "bgRed"
-    },
-    {
-        id: 2,
-        cssName: "green",
-        bgProp: "bgGreen"
-    },
-    {
-        id: 3,
-        cssName: "yellow",
-        bgProp: "bgYellow"
-    },
-    {
-        id: 4,
-        cssName: "purple",
-        bgProp: "bgPurple"
-    },
-    {
-        id: 5,
-        cssName: "naviBlue",
-        bgProp: "bgBlue"
-    },
-    {
-        id: 6,
-        cssName: "pink",
-        bgProp: "bgPink"
-    },
-]
 
 export const ColorPallet = ({ setShowColorPallet, noteId, note, setNoteData }) => {
     const { dispatch } = useData();
---
+++ src/components/ColorPallet/palletData.js
@@ -0,0 +1,32 @@
+export const colorPalletData = [
+    {
+        id: 1,
+        cssName: "red",
+        bgProp: "bgRed"
+    },
+    {
+        id: 2,
+        cssName: "green",
+        bgProp: "bgGreen"
+    },
+    {
+        id: 3,
+        cssName: "yellow",
+        bgProp: "bgYellow"
+    },
+    {
+        id: 4,
+        cssName: "purple",
+        bgProp: "bgPurple"
+    },
+    {
+        id: 5,
+        cssName: "naviBlue",
+        bgProp: "bgBlue"
+    },
+    {
+        id: 6,
+        cssName: "pink",
+        bgProp: "bgPink"
+    },
+]
